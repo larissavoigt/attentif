e3077a318dd1a08bc517ae4f4963e3d701909aa2
Dimitri D. <DimitriDok@users.noreply.github.com>
2015-11-08 15:14:48 -0500

    Changed home.js the function to remove color change.

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,10 +2,6 @@
 var control_happiness;
 var output_happiness;
 
-//declare the colors
-var red = 255;
-var green = 255;
-
 function initialize() {
 	//Get the hapiness control and its output by id
 	control_happiness = document.getElementById('control_happiness');
@@ -20,53 +16,40 @@ function update_happiness(){
 	
 	output_happiness.innerHTML = control_happiness.value;
 
-	if(control_happiness.value == 0){
-		output_happiness.innerHTML = "Depressed";
+	if(control_happiness.value <= 5){
+		output_happiness.innerHTML = "Desperately Unhappy";
 	}
-	else if(control_happiness.value <= 20){
-		output_happiness.innerHTML = "Very Sad";
+	else if(control_happiness.value <= 15){
+		output_happiness.innerHTML = "Very Unhappy";
 	}
-	else if(control_happiness.value <= 40){
-		output_happiness.innerHTML = "Sad";
+	else if(control_happiness.value <= 25){
+		output_happiness.innerHTML = "Unhappy";
 	}
-	else if(control_happiness.value <= 60){
+	else if(control_happiness.value <= 35){
+		output_happiness.innerHTML = "Passable";
+	}
+	else if(control_happiness.value <= 45){
+		output_happiness.innerHTML = "Quite OK";
+	}
+	else if(control_happiness.value <= 55){
 		output_happiness.innerHTML = "OK";
 	}
-	else if(control_happiness.value <= 80){
+	else if(control_happiness.value <= 65){
+		output_happiness.innerHTML = "Content";
+	}
+	else if(control_happiness.value <= 75){
+		output_happiness.innerHTML = "Cheerful";
+	}
+	else if(control_happiness.value <= 85){
 		output_happiness.innerHTML = "Happy";
 	}
-	else if(control_happiness.value <= 99){
-		output_happiness.innerHTML = "Very Happy";
-	}
-	else{
+	else if(control_happiness.value <= 95){
 		output_happiness.innerHTML = "Delighted";
 	}
-	
-	output_happiness.innerHTML += ": " + control_happiness.value + "%";
-	
-	if(control_happiness.value >=50){
-		green = 255;
-		red = lerp(255, 0, control_happiness.value/50) + 255;
-	}  
-	else{
-		green = lerp(0, 255, control_happiness.value/50);
-		red = 255;
+	else if(control_happiness.value == 100){
+		output_happiness.innerHTML = "Blissful";
 	}
-	
-	output_happiness.style.color = "rgb(" + red + "," + green + ", 0)";
-	
 }
-
-//This function was retrieved from the Linear Interpolation Wikipedia entry
-function lerp(v0, v1, t) {
-  return (1-t)*v0 + t*v1;
-}
-
 
 //Register the window on load callback
 window.onload = initialize;
-
-/*
-Exception: SyntaxError: expected expression, got '='
-@Scratchpad/1:26
-*/
